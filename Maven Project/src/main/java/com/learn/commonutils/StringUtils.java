dd92e607327103ba95aa10f2a769c02faafa43c8
ImalkaPerera <hpsimalkaperera@gmail.com>
2026-04-30 14:10:51 +0530

    fix: update greeting message in StringUtils to remove 'back'

--- Maven Project/src/main/java/com/learn/commonutils/StringUtils.java
+++ Maven Project/src/main/java/com/learn/commonutils/StringUtils.java
@@ -2,6 +2,6 @@ package com.learn.commonutils;
 
 public class StringUtils {
     public static String greet(String name) {
-        return "Hello welcome back, " + name;
+        return "Hello welcome , " + name;
     }
 }
